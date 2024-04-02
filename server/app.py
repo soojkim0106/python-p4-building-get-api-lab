72fb8a7b5a07e1ca09e353feee80989f5e71b290
Soojungkim <soojung.kimm@gmail.com>
2024-04-01 18:50:38 -0700

    complete lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,30 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    #! make_response useful for if you want to set actual headers
+    return bakeries, 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    return bakery.to_dict()
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        baked_goods.append(baked_good.to_dict())
+    
+    return baked_goods
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return (baked_good.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
